7e79f1071d78f81a99770827edfcfe06e5fd2e76
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-04-17 19:17:55 +0600

    Exp1 updated with thermal and random noise. removed white noise

--- Exp1_CommNoises.m
+++ Exp1_CommNoises.m
@@ -21,37 +21,50 @@ impulse_values = 5 * (2*randi([0 1], 1, 50) - 1); % Generates -5 or 5
 impulse_noise(impulse_positions) = impulse_values;
 noisy_signal_impulse = clean_signal + impulse_noise;
 
-% 3. White Noise
-white_noise = -1 + 2*rand(1, fs);  % Uniform distribution between -1 and 1
-noisy_signal_white = clean_signal + white_noise;
+% 3. Thermal Noise
+thermal_mean = 0;
+thermal_variance = 0.05;
+thermal_noise = thermal_mean + sqrt(thermal_variance) * randn(1, fs);
+noisy_signal_thermal = clean_signal + thermal_noise;
+
+% 4. Random Noise
+random_noise = -1 + 2*rand(1, fs);  % Uniform distribution between -1 and 1
+noisy_signal_random = clean_signal + random_noise;
 
 % Plotting
 figure;
 
-subplot(4,1,1);
+subplot(5,1,1);
 plot(t, clean_signal, 'LineWidth', 1.2);
 title('Clean Signal (Sine Wave)');
 xlabel('Time');
 ylabel('Amplitude');
 grid on;
 
-subplot(4,1,2);
+subplot(5,1,2);
 plot(t, noisy_signal_gaussian, 'LineWidth', 1.2);
 title('Noisy Signal with Gaussian Noise');
 xlabel('Time');
 ylabel('Amplitude');
 grid on;
 
-subplot(4,1,3);
+subplot(5,1,3);
 plot(t, noisy_signal_impulse, 'LineWidth', 1.2);
 title('Noisy Signal with Impulse Noise');
 xlabel('Time');
 ylabel('Amplitude');
 grid on;
 
-subplot(4,1,4);
-plot(t, noisy_signal_white, 'LineWidth', 1.2);
-title('Noisy Signal with White Noise');
+subplot(5,1,4);
+plot(t, noisy_signal_thermal, 'LineWidth', 1.2);
+title('Noisy Signal with Thermal Noise');
+xlabel('Time');
+ylabel('Amplitude');
+grid on;
+
+subplot(5,1,5);
+plot(t, noisy_signal_random, 'LineWidth', 1.2);
+title('Noisy Signal with Random Noise');
 xlabel('Time');
 ylabel('Amplitude');
 grid on;
@@ -59,8 +72,10 @@ grid on;
 % SNR Calculation
 snr_gaussian = 10 * log10(var(clean_signal) / var(gaussian_noise));
 snr_impulse  = 10 * log10(var(clean_signal) / var(impulse_noise));
-snr_white    = 10 * log10(var(clean_signal) / var(white_noise));
+snr_thermal  = 10 * log10(var(clean_signal) / var(thermal_noise));
+snr_random   = 10 * log10(var(clean_signal) / var(random_noise));
 
 fprintf('Signal-to-Noise Ratio (SNR) with Gaussian Noise: %.2f dB\n', snr_gaussian);
 fprintf('Signal-to-Noise Ratio (SNR) with Impulse Noise: %.2f dB\n', snr_impulse);
-fprintf('Signal-to-Noise Ratio (SNR) with White Noise: %.2f dB\n', snr_white);
+fprintf('Signal-to-Noise Ratio (SNR) with Thermal Noise: %.2f dB\n', snr_thermal);
+fprintf('Signal-to-Noise Ratio (SNR) with Random Noise: %.2f dB\n', snr_random);
